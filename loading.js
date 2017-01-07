5afffccff9e9ec88c2d8fd4d7d74c80d9906bd5d
Tafari Duncan <tafari.duncan@tufts.edu>
2017-01-07 18:38:49 -0500

    New Home page 90% done!

--- loading.js
+++ loading.js
@@ -2,7 +2,7 @@ function hideloader(){
 	setTimeout(function () {
 		document.getElementById("loading_box").style.display = "none";
 		document.getElementById("loading").style.display = "none";
-	}, 5000);
+	}, 2500);
 }
 /*
 $(window).load(function() {
