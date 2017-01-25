4294c7e22ff93e73400bb82a6e1a6f7a59993de7
Tafari Duncan <tafari.duncan@tufts.edu>
2017-01-25 09:55:17 -0500

    Need more time :P

--- construction.js
+++ construction.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var deadline = 'January 23 2017 00:00:00 GMT+05:00';
+var deadline = 'January 31 2017 00:00:00 GMT+05:00';
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.now();
